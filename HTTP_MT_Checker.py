e97f035e05ab368acaf00feb8e99365c4737c74a
yaksa <bemo@live.cn>
2019-12-04 16:16:07 +0800

    Add files via upload

---
+++ HTTP_MT_Checker.py
@@ -0,0 +1,38 @@
+#coding=utf-8
+import requests
+import sys
+import re
+
+print("Example: https://www.example.com")
+print()
+print()
+
+url = input("url:")
+
+r = requests.get(url)
+if r.status_code == 200:
+    print('surport: GET')
+
+r = requests.post(url)
+if r.status_code == 200:
+    print('surport: POST')
+    
+r = requests.put(url)
+if r.status_code == 200:
+    print('surport: PUT')
+    
+r = requests.delete(url)
+if r.status_code == 200:
+    print('surport: DELETE')
+    
+r = requests.head(url)
+if r.status_code == 200:
+    print('surport: HEAD')
+    
+r = requests.options(url)
+if r.status_code == 200:
+    print('surport: OPTIONS')
+
+print()
+print("CHECK OVER!")
+input()
