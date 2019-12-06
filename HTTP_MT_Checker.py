b2dea4cbb56d7063bce93a56164c14b9f5e45aa9
yaksa <bemo@live.cn>
2019-12-06 20:24:20 +0800

    Update HTTP_MT_Checker.py

--- HTTP_MT_Checker.py
+++ HTTP_MT_Checker.py
@@ -6,32 +6,33 @@ print("Example: https://www.example.com")
 print()
 print()
 
-url = input("url:")
+while 1:
+    url = input("url:")
 
-r = requests.get(url)
-if r.status_code == 200:
-    print('surport: GET')
+    r = requests.get(url)
+    if r.status_code < 299:
+        print('surport: GET')
 
-r = requests.post(url)
-if r.status_code == 200:
-    print('surport: POST')
+    r = requests.post(url)
+    if r.status_code < 299:
+        print('surport: POST')
     
-r = requests.put(url)
-if r.status_code == 200:
-    print('surport: PUT')
+    r = requests.put(url)
+    if r.status_code < 299:
+        print('surport: PUT')
     
-r = requests.delete(url)
-if r.status_code == 200:
-    print('surport: DELETE')
+    r = requests.delete(url)
+    if r.status_code < 299:
+        print('surport: DELETE')
     
-r = requests.head(url)
-if r.status_code == 200:
-    print('surport: HEAD')
+    r = requests.head(url)
+    if r.status_code < 299:
+        print('surport: HEAD')
     
-r = requests.options(url)
-if r.status_code == 200:
-    print('surport: OPTIONS')
+    r = requests.options(url)
+    if r.status_code < 299:
+        print('surport: OPTIONS')
 
-print()
-print("CHECK OVER!")
-input()
+    print()
+    print("CHECK OVER!")
+    print()
